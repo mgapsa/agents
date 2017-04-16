667de0456caa3e53a6e30bf28dbc7aff7fc54e46
mgapsa <gapsamikolaj@gmail.com>
2017-04-17 00:49:47 +0200

    sample project implemented

--- src/main/java/com/pw/sag/agents/sample/SampleAgent.java
+++ src/main/java/com/pw/sag/agents/sample/SampleAgent.java
@@ -1,4 +1,4 @@
-package com.pw.sag.agents;
+package com.pw.sag.agents.sample;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
---
+++ src/main/java/com/pw/sag/messages/IntegerMessageContentReceiver.java
@@ -0,0 +1,5 @@
+package com.pw.sag.messages;
+
+public interface IntegerMessageContentReceiver {
+    void onMessage(Integer message);
+}
